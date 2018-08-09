2b35a8f1758f41af803f47dc7afc511e0fc8393f
arujit <arujit.pradhan@go-mmt.com>
2018-08-09 10:48:41 +0530

    Added entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 envsubst < ./connect-properties/connect-standalone.properties.template > ./connect-properties/connect-standalone.properties
-envsubst < ./connect-properties/sink.properties.template > ./connect-properties/sink.properties
+envsubst < ./connect-properties/redis-connect.conf.template > ./connect-properties/redis-connect.conf
 envsubst < ./newrelic/newrelic.yml.template > ./newrelic/newrelic.yml
 
 CMD=${1:-"exit 0"}
 if [[ "xxx$CMD" == "xxx" ]]
 then
     echo "Starting Connect..."
-    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/sink.properties
+    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/redis-connect.conf
 else
     /bin/bash -c "$*"
 fi
