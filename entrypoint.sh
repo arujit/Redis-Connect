46de6718a2ebb8f12cbd9bd73199a3461f7358ef
arujit <arujit.pradhan@go-mmt.com>
2018-08-09 10:43:41 +0530

    Added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+envsubst < ./connect-properties/connect-standalone.properties.template > ./connect-properties/connect-standalone.properties
+envsubst < ./connect-properties/sink.properties.template > ./connect-properties/sink.properties
+envsubst < ./newrelic/newrelic.yml.template > ./newrelic/newrelic.yml
+
+CMD=${1:-"exit 0"}
+if [[ "xxx$CMD" == "xxx" ]]
+then
+    echo "Starting Connect..."
+    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/sink.properties
+else
+    /bin/bash -c "$*"
+fi
