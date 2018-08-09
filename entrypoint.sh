0bb99ec5394eb5bf79f60d049d4f1cdd6d689654
arujit <arujit.pradhan@go-mmt.com>
2018-08-09 13:52:08 +0530

    updated logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 envsubst < ./connect-properties/connect-standalone.properties.template > ./connect-properties/connect-standalone.properties
-envsubst < ./connect-properties/redis-connect.conf.template > ./connect-properties/redis-connect.conf
+envsubst < ./connect-properties/sink.properties.template > ./connect-properties/sink.properties
 envsubst < ./newrelic/newrelic.yml.template > ./newrelic/newrelic.yml
 
-CMD=${1:-"exit 0"}
-if [[ "xxx$CMD" == "xxx" ]]
+if [[ "$#" -eq 0 ]]
 then
     echo "Starting Connect..."
-    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/redis-connect.conf
+    exec /opt/kafka_2.11-${KAFKA_VERSION}/bin/connect-standalone.sh ./connect-properties/connect-standalone.properties ./connect-properties/sink.properties
 else
     /bin/bash -c "$*"
 fi
--- src/main/scala/com/goibibo/connect/redis/RedisSinkConnector.scala
+++ src/main/scala/com/goibibo/connect/redis/RedisSinkConnector.scala
@@ -17,18 +17,14 @@ class RedisSinkConnector extends SinkConnector {
     var props: Map[String, String] = _
 
     override def start(props: util.Map[String, String]): Unit = {
-        def apply(property: String): String = {
-            val newVariable = property.replace("${","").replace("}","")
-            val envVariables = sys.env.get(newVariable)
-            val sysProperties = sys.props.get(newVariable)
-            val newValue = envVariables.getOrElse(sysProperties.getOrElse(property))
-            newValue
-        }
-        val properties = props.asScala.toMap
-        val newProperties = {
-            properties.mapValues { value => if (value.startsWith("${") && value.endsWith("}")) apply(value) else value }
-        }
-        this.props = newProperties
+        this.props = props.asScala.mapValues { value =>
+                if(value.startsWith("${") && value.endsWith("}")) {
+                    val newVariable = value.replace("${", "").replace("}", "")
+                    val envVariables: Option[String] = sys.env.get(newVariable)
+                    val sysProperties: Option[String] = sys.props.get(newVariable)
+                    envVariables.getOrElse(sysProperties.getOrElse(value))
+                } else value
+        }.toMap
     }
 
     override def taskClass(): Class[RedisSinkTask] = classOf[RedisSinkTask]
